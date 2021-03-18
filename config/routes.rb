392bd15abcf25bb394f3bf6d4559222dda7426db
stacy <stacydn23@gmail.com>
2021-03-18 00:00:05 -0400

    commit tree c8b60b575ff53b910e06c5b9c8012bae332682b1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +54,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  resources :coupons, only: [:index, :new, :show, :create]
+
+  
 end
